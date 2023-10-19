5d5d047c2b3e1f105ea5dc487e9cac4854c9b87d
Esdras <esosofs@miumg.edu.gt>
2023-10-18 21:56:47 -0600

    cambios para jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,25 @@ import { UsersModule } from '../users/users.module';
 import { CONSTANTS } from '../configuration/constants';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: CONSTANTS.SECRET,
-      signOptions: { expiresIn: CONSTANTS.EXPIRE },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+        global: true,
+      }),
+      inject: [ConfigService],
     }),
+    // JwtModule.register({
+    //   global: true,
+    //   secret: CONSTANTS.SECRET,
+    //   signOptions: { expiresIn: CONSTANTS.EXPIRE },
+    // }),
   ],
   controllers: [AuthController],
   providers: [
@@ -23,6 +33,7 @@ import { AuthGuard } from './auth.guard';
       useClass: AuthGuard,
     },
     AuthService
-  ]
+  ],
+  exports: [JwtModule],
 })
 export class AuthModule {}
